24f314295f7af542b9d3cf43cfda3de96541a193
SymbolixAU <dcooley@Symbolix.com.au>
2018-07-31 20:53:31 +1000

    tests

--- tests/testthat/test-paths.R
+++ tests/testthat/test-paths.R
@@ -7,7 +7,7 @@ test_that("multilinestrings parsed", {
 	df <- data.frame(id = 1:3, val = letters[1:3])
 	df$polyline <- polylines
 
-	df_res <- mapdeck:::unlistMultiPolyline( df, 'polyline' )
+	df_res <- mapdeck:::unlistMultiGeometry( df, 'polyline' )
 
 	expect_true(all(df_res$polyline == unlist(polylines)))
 	expect_true(all(df_res$val == c("a","a","a","b","b","c")))
