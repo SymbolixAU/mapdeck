1e4606f6439da7611a6a2f4dcee4ace43d3ec47c
SymbolixAU <dcooley@symbolix.com.au>
2018-10-25 15:32:09 +1100

    example for #65

--- R/scratch.R
+++ R/scratch.R
@@ -276,3 +276,66 @@
 # 		, tooltip = 'info'
 # 		, legend = F
 # 	)
+
+
+# library(shiny)
+# library(shinydashboard)
+# library(mapdeck)
+# set_token(read.dcf("~/.googleAPI", fields = "MAPBOX"))
+#
+# ui <- dashboardPage(
+# 	dashboardHeader()
+# 	, dashboardSidebar()
+# 	, dashboardBody(
+# 		mapdeckOutput(
+# 			outputId = "map1"
+# 		)
+# 		, mapdeckOutput(
+# 			outputId = "map2"
+# 		)
+# 	)
+# )
+#
+# server <- function(input, output) {
+#
+# 	df <- melbourne
+# 	df$elevation <- sample(200:5000, size = nrow(df))
+# 	df$info <- paste0("<b>SA2 - </b><br>",df$SA2_NAME)
+#
+# 	output$map1 <- renderMapdeck({
+# 		mapdeck(
+# 			style = mapdeck_style('dark')
+# 			, location = c(145, -38)
+# 			, zoom = 8
+# 		) %>%
+# 			add_polygon(
+# 				data = df
+# 				, polyline = "geometry"
+# 				, layer = "polygon_layer"
+# 				, fill_colour = "SA2_NAME"
+# 				, elevation = "elevation"
+# 				, stroke_width = 0
+# 				, tooltip = 'info'
+# 				, legend = T
+# 			)
+# 	})
+#
+# 	output$map2 <- renderMapdeck({
+# 		mapdeck(
+# 			style = mapdeck_style('dark')
+# 			, location = c(145, -38)
+# 			, zoom = 8
+# 		) %>%
+# 			add_polygon(
+# 				data = df
+# 				, polyline = "geometry"
+# 				, layer = "polygon_layer"
+# 				, fill_colour = "SA3_NAME"
+# 				, elevation = "elevation"
+# 				, stroke_width = 0
+# 				, tooltip = 'info'
+# 				, legend = T
+# 			)
+# 	})
+# }
+# shinyApp(ui, server)
