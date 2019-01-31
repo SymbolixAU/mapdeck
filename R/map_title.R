0eed3ab8b9a80a77e36ef61147e9554d0d39e79d
SymbolixAU <dcooley@symbolix.com.au>
2019-01-31 15:09:51 +1100

    docs

--- R/map_title.R
+++ R/map_title.R
@@ -1,16 +1,15 @@
 #' Add Title
 #'
+#' Adds a title to a map
+#'
+#' @inheritParams add_arc
+#' @param title
 #'
 #' @export
 add_title <- function(map, title, layer_id = NULL) {
 
-	layer_id <- layerId( layer_id )
-
-	# title <- list(
-	# 	title = "my title"
-	# 	#, position = "TOP_RIGHT"
-	# 	#, css = "background: #00FF00;"
-	# )
+	layer_id <- layerId( layer_id, layer = "title" )
+	if(!inherits(title, "list")) title <- list(title = title)
 
 	invoke_method(map, "add_title", layer_id, jsonify::to_json( title, unbox = TRUE ) )
 }
