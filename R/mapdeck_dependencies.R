41d32213a53a5344e14db78fe8851af8d2e24904
tospig <david.cooley141@gmail.com>
2019-07-27 20:26:14 +1000

    deck.gl v7.1.10

--- R/mapdeck_dependencies.R
+++ R/mapdeck_dependencies.R
@@ -157,7 +157,7 @@ deckgl_min_js <- function() {
 	list(
 		createHtmlDependency(
 			name = "deckgl",
-			version = "7.1.3",
+			version = "7.1.10",
 			src = system.file("htmlwidgets/lib/", package = "mapdeck"),
 			script = c("deckgl.min.js"),
 			all_files = FALSE
