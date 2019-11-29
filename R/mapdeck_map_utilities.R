9fb93cd056bf5936ab0f1a40130552af1c9885dc
Mitchell <mitch.ohara-wild@monash.edu>
2019-11-29 11:47:07 +1100

    Added icon layer support

--- R/mapdeck_map_utilities.R
+++ R/mapdeck_map_utilities.R
@@ -138,7 +138,7 @@ layerId <- function(
 
 mapdeck_layers <- function() {
 	c("arc", "bitmap", "column", "geojson", "greatcircle","grid","heatmap","hexagon",
-		"line", "mesh", "path","pointcloud", "polygon","scatterplot", "screengrid",
+		"icon", "line", "mesh", "path","pointcloud", "polygon","scatterplot", "screengrid",
 		"text", "title","trips")
 }
 
