ea6ffd4b5ee13beb7b301c209369c0f940368858
David <david.cooley141@gmail.com>
2020-04-19 19:52:08 +1000

    POC #291

--- R/map_layer_terrain.R
+++ R/map_layer_terrain.R
@@ -30,7 +30,7 @@ mapdeckTerrainDependency <- function() {
 #' texture <- 'https://raw.githubusercontent.com/visgl/deck.gl-data/master/website/terrain-mask.png'
 #' bounds <- c(-122.5233, 37.6493, -122.3566, 37.8159)
 #'
-#' mapdeck( location = bounds[1:2], zoom = 10) %>%
+#' mapdeck() %>%
 #'   add_terrain(
 #'     elevation_data = elevation,
 #'     texture = texture,
@@ -45,14 +45,27 @@ add_terrain <- function(
 	layer_id = NULL,
 	elevation_data,
 	texture = NULL,
-	bounds
+	bounds,
+	update_view = TRUE,
+	focus_layer = FALSE
 	) {
+
+	bbox <- init_bbox()
+	bbox[[1]] <- bounds[1:2]
+	bbox[[2]] <- bounds[3:4]
 
 	map <- addDependency( map, mapdeckTerrainDependency() )
 	layer_id <- layerId( layer_id, "terrain" )
 
+	print(elevation_data)
+	print(texture)
+	print(update_view)
+	print(bounds)
+	print(bbox)
+	print(focus_layer)
+
 	invoke_method(
-		map, "add_terrain", layer_id, elevation_data, texture, bounds
+		map, "add_terrain", layer_id, elevation_data, texture, update_view, bounds, bbox, focus_layer
 	)
 }
 
--- inst/htmlwidgets/lib/terrain/terrain.js
+++ inst/htmlwidgets/lib/terrain/terrain.js
@@ -1,20 +1,26 @@
 
+function add_terrain(map_id, layer_id, elevation_data, texture, update_view, bounds, bbox, focus_layer ) {
 
-function add_terrain(map_id, layer_id, elevation_data, texture, bounds ) {
+  console.log( layer_id );
+  console.log( elevation_data );
+  console.log( texture );
+  console.log( update_view );
+  console.log( bounds );
+  console.log( bbox );
 
   const terrainLayer = new deck.TerrainLayer({
+  	id: "terrain-"+layer_id,
   	elevationDecoder: {
       rScaler: 2,
       gScaler: 0,
       bScaler: 0,
       offset: 0
     },
-    // Digital elevation model from https://www.usgs.gov/
     elevationData: elevation_data,
     texture: texture,
     bounds: bounds,
   });
 
   md_update_layer( map_id, 'terrain-'+layer_id, terrainLayer );
-  //md_layer_view( map_id, layer_id, focus_layer, bbox, update_view );
+  md_layer_view( map_id, map_type, layer_id, focus_layer, bbox, update_view );
 }
