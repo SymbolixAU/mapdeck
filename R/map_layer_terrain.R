01314d847fcc5204b6575312e5c3ddeeb845c9de
SymbolixAU <dcooley@symbolix.com.au>
2020-04-21 07:51:52 +1000

    examples and docs

--- R/map_layer_terrain.R
+++ R/map_layer_terrain.R
@@ -16,11 +16,33 @@ mapdeckTerrainDependency <- function() {
 #' Adds mesh surfaces from height map images
 #'
 #' @inheritParams add_arc
-#' @param elevation_data Image URL that encodes height data.
+#' @param elevation_data Image URL that encodes height data. When \code{elevation_data}
+#' is a URL template, i.e. a string containing '{x}' and '{y}', it loads terrain tiles on demand
+#' and renders a mesh for each tile. If \code{elevation_data} is an absolute URL, as ingle mesh is used,
+#' and the \code{bounds} argument is required to position it into the world space.
 #' @param texture Image URL to use as the texture
-#' @param elevation_decoder Numeric vector giving parameters to use to convert a
-#' pixel to elevation in metres.
-#' @param bounds
+#' @param elevation_decoder Four value used to convert a pixel to elevation in metres. The
+#' values correspond to rScale, gScale, bScale, offset. See details
+#' @param bounds Four values (\code{ c(left, bottom, right, top) }. bounds of the
+#' image to fit in x,y coordinates into. \code{left} and \code{right} referes to the world
+#' longitude/x at the corresponding side of the image. \code{top} and \code{bottom} refers to
+#' the world latitude/y at the corresponding side of the image. Must be supplied when using
+#' non-tiled \code{elevation_data}
+#' @param max_error Martini error tolerance in metres, smaller number results in more detailed mesh.
+#'
+#' @details
+#'
+#' The \code{elevation_decoder} contains four values representing
+#' \itemize{
+#'   \item{rScale - Multiplier of the red channel}
+#'   \item{gScale - Multiplier of the green channel}
+#'   \item{bScale - Multiplier of the blue channel}
+#'   \item{offset - translation of the sum}
+#' }
+#'
+#' Each colour channel is a number between [0, 255].
+#'
+#'
 #' @examples
 #' \donttest{
 #'
@@ -32,9 +54,10 @@ mapdeckTerrainDependency <- function() {
 #'
 #' mapdeck() %>%
 #'   add_terrain(
-#'     elevation_data = elevation,
-#'     texture = texture,
-#'     bounds = bounds
+#'     , elevation_data = elevation
+#'     , elevation_decoder = c(1,0,0,0)
+#'     , texture = texture
+#'     , bounds = bounds
 #'   )
 #'
 #' }
@@ -45,20 +68,27 @@ add_terrain <- function(
 	layer_id = NULL,
 	elevation_data,
 	texture = NULL,
-	bounds,
+	elevation_decoder = c(1,0,0,0),
+	bounds = NULL,
+	max_error = 4,
 	update_view = TRUE,
 	focus_layer = FALSE
 	) {
 
+	experimental_layer("terrain")
+
 	bbox <- init_bbox()
-	bbox[[1]] <- bounds[1:2]
-	bbox[[2]] <- bounds[3:4]
+	if( !is.null( bounds ) ) {
+		bbox[[1]] <- bounds[1:2]
+		bbox[[2]] <- bounds[3:4]
+	}
 
 	map <- addDependency( map, mapdeckTerrainDependency() )
 	layer_id <- layerId( layer_id, "terrain" )
 
 	invoke_method(
-		map, "add_terrain", map_type( map ), layer_id, elevation_data, texture, update_view, bounds, bbox, focus_layer
+		map, "add_terrain", map_type( map ), layer_id, elevation_data, texture, elevation_decoder,
+		max_error, update_view, bounds, bbox, focus_layer
 	)
 }
 
--- inst/htmlwidgets/lib/terrain/terrain.js
+++ inst/htmlwidgets/lib/terrain/terrain.js
@@ -1,18 +1,17 @@
-function add_terrain(map_id, map_type, layer_id, elevation_data, texture, update_view, bounds, bbox, focus_layer ) {
+function add_terrain(map_id, map_type, layer_id, elevation_data, texture, elevation_decoder, max_error, update_view, bounds, bbox, focus_layer ) {
 
   const terrainLayer = new deck.TerrainLayer({
   	id: "terrain-"+layer_id,
-  	/*
   	elevationDecoder: {
-      rScaler: 1,
-      gScaler: 1,
-      bScaler: 1,
-      offset: 1
-    },
-    */
+  	  rScaler: elevation_decoder[0],
+  	  gScaler: elevation_decoder[1],
+  	  bScaler: elevation_decoder[2],
+  	  offset: elevation_decoder[3]
+  	  },
     elevationData: elevation_data,
     texture: texture,
     bounds: bounds,
+    meshMaxError: max_error
   });
 
   md_update_layer( map_id, 'terrain-'+layer_id, terrainLayer );
