4db1b12d33ca3cf60fecaf65f56ee948558581e3
David <david.cooley141@gmail.com>
2020-05-17 13:09:55 +1000

    deckgl 8.1.6

--- R/mapdeck_dependencies.R
+++ R/mapdeck_dependencies.R
@@ -161,7 +161,7 @@ deckgl_min_js <- function() {
 	list(
 		createHtmlDependency(
 			name = "deckgl",
-			version = "8.1.0",
+			version = "8.1.6",
 			src = system.file("htmlwidgets/lib/", package = "mapdeck"),
 			script = c("deckgl.min.js"),
 			all_files = FALSE
