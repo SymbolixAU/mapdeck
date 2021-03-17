ed8cfd89bff4f328263679c484de74ce74835d10
dcooley <david.cooley141@gmail.com>
2021-03-17 21:01:12 +1100

    some tests

--- tests/testthat/tests-interleave.R
+++ tests/testthat/tests-interleave.R
@@ -45,3 +45,68 @@ test_that("interleaving primitives (POINT, LINE, TRIANGLE) return the same inter
 
 
 })
+
+
+test_that("polygon multicoloured coordinates are coloured correctly", {
+
+	df <- data.frame(
+		x = c(0,1,1,0,0)
+		, y = c(0,0,1,1,0)
+		, z = 1:5
+		, s = 1:5
+	)
+
+	l <- list(fill_colour = "s")
+
+	sf_square <- sfheaders::sf_polygon(
+		obj = df
+		, x = "x"
+		, y = "y"
+		, z = "z"
+		, list_columns = "s"
+		, keep = TRUE
+	)
+
+	## why does this crash??
+	## because it's expecting a LINESTRING??
+	# res <- mapdeck:::rcpp_path_interleaved(
+	# 	sf = sf_square
+	# 	, params = l
+	# 	, list_columns = 1L
+	# 	, digits = 6L
+	# 	, layer_name = "polygon"
+	# )
+
+	# mapdeck:::rcpp_interleave_primitive_triangle(
+	# 	data = sf_square
+	# 	, list_columns = integer()
+	# )
+
+	res <- mapdeck:::rcpp_triangle_interleaved(
+		data = sf_square
+		, params = l
+		, list_columns = integer()
+		, digits = 6L
+		, layer_name = "polygon"
+	)
+
+	res_df <- jsonify::from_json( res[["data"]] )
+
+	res_colours <- colourvalues::convert_colours( matrix(res_df$data$fill_colour, ncol = 4, byrow = TRUE) * 255.0 )
+
+	expect_true( length( res_colours ) == 6 ) ## because there are 2 triangles
+
+	## The first coordinate in sf_squares is 0,0 , and therefore so is the last one.
+
+ 	# res_coords <- matrix( res_df$coordinates, ncol = 3, byrow = TRUE )
+	# input_matrix <- as.matrix( df[, c("x", "y", "z" ) ] )
+
+	# mapdeck:::rcpp_interleave_primitive_triangle(
+	# 	data = sf_square
+	# 	, list_columns = integer()
+	# )
+
+})
+
+
+
